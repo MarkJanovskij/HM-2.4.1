06363d031c59214bd48201ef3cde347d233457f1
Mark <markyanovskiy@gmail.com>
2023-12-08 15:14:59 +0100

    Fix

--- yearlyExpences.js
+++ yearlyExpences.js
@@ -6,23 +6,13 @@
 // Создать тестовую функцию, которая будет проверять, что расчет суммы расходов производится верно. Проверить расчет на данных тестовых объектах (используйте forEach()):
 
 
-let expencesExamples = [
-    { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
-    { yearlyExpences: [500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200] },
-    { yearlyExpences: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000] },
-   ];
+// let expencesExamples = [
+//     { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
+//     { yearlyExpences: [500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200] },
+//     { yearlyExpences: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000] },
+//    ];
 
-   let sum = 0;
-expencesExamples.forEach(expence => {
-    let sum = 0;
-    expence.yearlyExpences.forEach(amount => {
-    if (amount > 1000) {
-        amount =amount -1000;
-        }
-        sum += amount;
-});
-})
-console.log(sum)
+   
 // let sum =0;
 //    expencesExamples.forEach(expence => {
 //     expence.yearlyExpences.forEach (amount =>{
@@ -31,8 +21,22 @@ console.log(sum)
     
 //    });
 //    console.log(sum)
+let expencesExamples = [
+    { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
+    { yearlyExpences: [500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200] },
+    { yearlyExpences: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000] },
+   ];
 
-
+   
+   let sum = 0;
+   expencesExamples.forEach(expense => {
+       expense.yearlyExpences.forEach(amount => {
+           if (amount > 1000) {
+               sum += amount;
+           }
+       });
+   });
+   console.log(sum);
 
 
 
